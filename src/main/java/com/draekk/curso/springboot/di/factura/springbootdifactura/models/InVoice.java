c2ab0233c79787ce952837bd21b223a5b26bbc28
draekk <rodriver1992@gmail.com>
2024-01-01 18:29:46 -0300

    Add getters and setters to InVoice, Item, and Product classes

--- src/main/java/com/draekk/curso/springboot/di/factura/springbootdifactura/models/InVoice.java
+++ src/main/java/com/draekk/curso/springboot/di/factura/springbootdifactura/models/InVoice.java
@@ -2,8 +2,14 @@ package com.draekk.curso.springboot.di.factura.springbootdifactura.models;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InVoice {
 
     private Client client;
     private String description;
+    private List<Item> items;
 }
--- src/main/java/com/draekk/curso/springboot/di/factura/springbootdifactura/models/Product.java
+++ src/main/java/com/draekk/curso/springboot/di/factura/springbootdifactura/models/Product.java
@@ -1,6 +1,12 @@
 package com.draekk.curso.springboot.di.factura.springbootdifactura.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Product {
 
-    
+    private String name;
+    private Integer price;
 }
